a65cfd99348086fa5d7f771b382b652c3e827a7c
OlegJak <100533386+OlegJak@users.noreply.github.com>
2022-08-22 21:23:11 +0300

    Add files via upload

---
+++ js/myScript.js
@@ -0,0 +1,39 @@
+let position = $(".img__container:last-child").offset();
+let countImg = $(".section__container").children().length
+let imgSrc = new Array()
+for (let i = 0; i < countImg; i++){
+    imgSrc[i] = $(".section__img").eq(i).attr("src")
+}
+
+$(document).scroll(function () { 
+    var screenTop = $(document).scrollTop();
+    position = $(".img__container:last-child").offset();
+    let y = 0;
+    if (screenTop >= position.top - 600){
+        for (y; y < countImg; y++){
+            $(".section__container").append($('<div class="img__container"></div>').append($('<img src="' + imgSrc[y] + '" class="section__img">')))
+        }
+    }
+});
+
+
+
+
+
+/*
+
+let img = $('<img src="img/img1.jpg" alt="" class="section__img">');
+
+let div = $('<div class="img__container"></div>');
+
+let element = div.append(img);
+
+$(document).scroll(function () { 
+    var screenTop = $(document).scrollTop();
+
+    if (screenTop >= position.top){
+        $(".section__container").append(element);
+    }
+});
+
+*/
